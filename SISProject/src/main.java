7bfed6cd7d4630e13f9e2e6f0cc803b9e6c0c6a4
colin.hart <colin.hart@MU-C1-01.mullen.pvt.k12.co.us>
2022-11-03 13:16:47 -0600

    DOG 3.0

--- SISProject/src/main.java
+++ SISProject/src/main.java
@@ -3,8 +3,9 @@ public class main{
 
 		public static void main(String[] args){
 			
-				System.out.println("yahoo");
-				System.out.println("iowhiorgq");
-				System.out.println("dog ");
+				System.out.println("RISE!!!");
+				System.out.println("REBEL!!!");
+				System.out.println("Viva La revolution!!!");
+				System.out.println("Para Gordo perros");
 			}
 }
