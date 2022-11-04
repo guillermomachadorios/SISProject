bf226d5619d3a7205280d22640ab5f6dbb30766b
colin.hart <colin.hart@MU-C1-01.mullen.pvt.k12.co.us>
2022-11-04 13:01:30 -0600

    SIS Project Student Pojo

--- SISProject/src/SubMenu.java
+++ SISProject/src/SubMenu.java
@@ -1,11 +1,9 @@
 
 public class SubMenu
 	{
-
-		public static void main(String[] args)
-			{
-				// TODO Auto-generated method stub
-
-			}
+	public static void subMenu() 
+		{
+		System.out.println();
+		}
 
 	}
--- SISProject/src/main.java
+++ SISProject/src/main.java
@@ -1,10 +1,8 @@
 
 public class main{
 
-		public static void main(String[] args){
-				System.out.println("RISE!!!");
-				System.out.println("REBEL!!!");
-				System.out.println("Viva La revolution!!!");
-				System.out.println("Para Gordo perros");
+		public static void main(String[] args)
+			{
+			
 			}
 }
