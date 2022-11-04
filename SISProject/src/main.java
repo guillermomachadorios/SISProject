fad486a4194d82058a837c220ec8f630f901c8a4
colin.hart <colin.hart@MU-C1-01.mullen.pvt.k12.co.us>
2022-11-04 13:07:27 -0600

    Student pojo for SIS project

--- SISProject/src/main.java
+++ SISProject/src/main.java
@@ -2,11 +2,7 @@
 public class main{
 		public static void main(String[] args)
 			{
-				System.out.println("Down with the leader");
-				System.out.println("Up with the dog");
-				System.out.println("Viva Revolution  ");
-				
-				
-				System.out.println("hh");
+		
 
+			}
 }
