73298feec0da9029b7fbfb4d7408a0a6070d57f2
colin.hart <colin.hart@MU-C1-01.mullen.pvt.k12.co.us>
2022-11-07 13:44:00 -0700

    psot conflict merge

--- SISProject/src/main.java
+++ SISProject/src/main.java
@@ -2,19 +2,14 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 public class main{
-<<<<<<< HEAD
 
-		public static void main(String[] args)
-			{
-			
-=======
 		public static void main(String[] args)
 			{		
 				System.out.println("yahoo");
 				System.out.println("iowhiorgq");
 				System.out.println("dog");
 				System.out.println("What up shlat");
->>>>>>> branch 'master' of https://github.com/guillermomachadorios/SISProject.git
+
 			}
       
 		public static void sorting() 
