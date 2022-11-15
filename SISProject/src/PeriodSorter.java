3cbd68147e1a7079801ddd533f97a4ecf44b9be2
cole.prendeville <cole.prendeville@MU-C1-20.mullen.pvt.k12.co.us>
2022-11-15 10:44:17 -0700

    Fixing some Variable Names

--- SISProject/src/PeriodSorter.java
+++ SISProject/src/PeriodSorter.java
@@ -5,6 +5,6 @@ public class PeriodSorter implements Comparator<Student>
     {
     public int compare(Song s1, Song s2)
         {
-            return s1.getPeriod().compareTo(s2.getPeriod());
+            return s1.getPeriodOne().compareTo(s2.getPeriodOne());
         }
     }
